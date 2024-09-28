0b32bbac6454e5e74ef803045ba23d8d5dfb5e9b
Boriska <boristheblade.dlebov@yandex.ru>
2024-09-28 12:34:07 +0300

    mypy  check

--- models/database.py
+++ models/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///recipes.db"
 
